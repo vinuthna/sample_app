c76a08012e14ff05a493a82a6c03c036368f9d3a
vinuthnagadde <vinnigadde@gmail.com>
2011-10-03 22:35:48 +0530

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,10 +4,17 @@ describe PagesController do
   render_views
 
   describe "GET 'home'" do
+  	
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+       get 'home'
+        response.should have_selector("title",
+                      :content => "Ruby on Rails Tutorial Sample App | Home")
+     end 
   end
 
   describe "GET 'contact'" do
@@ -15,6 +22,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+       get 'contact'
+        response.should have_selector("title",
+                      :content => "Ruby on Rails Tutorial Sample App | Contact")
+     end 
   end
 
   describe "GET 'about'" do
@@ -22,5 +34,10 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    it "should have the right title" do
+       get 'about'
+        response.should have_selector("title",
+                      :content => "Ruby on Rails Tutorial Sample App | About")
+     end 
   end
 end
