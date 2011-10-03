1b8e8138f3e1b9dbd1f0286a5df9d1fb3cd85a0b
vinuthnagadde <vinnigadde@gmail.com>
2011-10-03 13:36:47 +0530

    Today Commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ Spork.prefork do
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
+  
   RSpec.configure do |config|
     # == Mock Framework
     #
