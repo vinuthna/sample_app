4e4bc834d51308c9b0c806af6cb79baa1502fecd
vinuthnagadde <vinnigadde@gmail.com>
2011-10-07 16:45:21 +0530

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
 	 root :to => 'pages#home'
 
   
@@ -7,7 +9,7 @@ SampleApp::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
    
-   
+   match '/signup',    :to => 'users#new'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
